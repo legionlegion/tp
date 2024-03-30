69361b2599ee06a7c9093d8b66b86b8ade968630
qinboan <e0486958@u.nus.edu>
2024-03-30 22:59:04 +0800

    update traceCommand

--- src/main/java/seedu/address/logic/commands/appointment/TraceCommand.java
+++ src/main/java/seedu/address/logic/commands/appointment/TraceCommand.java
@@ -1,10 +1,14 @@
 package seedu.address.logic.commands.appointment;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.appointment.Appointment;
+
+import java.util.List;
 
 import static java.util.Objects.requireNonNull;
 
@@ -16,19 +20,29 @@ public class TraceCommand extends Command {
         + "appointment identified.\n"
         + "Example: " + COMMAND_WORD + " 1 ";
 
-    public static final String MESSAGE_TRACE_SUCCESS = "Traced appointments and persons listed!";
-    private final Index index;
+    public static final String MESSAGE_TRACE_SUCCESS = "Traced appointment: %1$s";
+    private final Index targetIndex;
 
     /**
      * @param index of the appointment in the filtered appointment list to edit
      */
     public TraceCommand(Index index) {
         requireNonNull(index);
-        this.index = index;
+        this.targetIndex = index;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        return new CommandResult(MESSAGE_TRACE_SUCCESS);
+        requireNonNull(model);
+        List<Appointment> lastShownList = model.getFilteredAppointmentList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_APPOINTMENT_DISPLAYED_INDEX);
+        }
+        Appointment appointmentToDelete = lastShownList.get(targetIndex.getZeroBased());
+        //model.updateFilteredAppointmentList();
+        //model.updateFilteredPersonList();
+        return new CommandResult(String.format(MESSAGE_TRACE_SUCCESS,
+            Messages.formatAppointment(appointmentToDelete)));
     }
 }
