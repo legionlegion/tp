92946c53db8387cd4a86e6f5883487b4a649e8b5
Sampson Ye Kaihang <sampsonye2017@gmail.com>
2024-04-13 23:23:00 +0800

    Update ImportCommandTest.java

--- src/test/java/seedu/address/logic/commands/person/ImportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/person/ImportCommandTest.java
@@ -55,7 +55,6 @@ public class ImportCommandTest {
         ImportCommand importCommand = new ImportCommand(WRONG_FORMAT_FILE_PATH);
         assertThrows(CommandException.class, () -> importCommand.execute(model));
     }
-  
     @Test
     public void execute_createPersonFromMap_success() {
         Map<String, String> patientDetail = new HashMap<>();
